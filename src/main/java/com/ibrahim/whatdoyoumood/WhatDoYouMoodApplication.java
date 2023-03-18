208e76ac3f1b3c88190de9101fe46152943b3428
ibrahimozturk <ozturkib16@itu.edu.tr>
2023-03-18 03:36:45 +0300

    First Commit

---
+++ src/main/java/com/ibrahim/whatdoyoumood/WhatDoYouMoodApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahim.whatdoyoumood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatDoYouMoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatDoYouMoodApplication.class, args);
+    }
+
+}
